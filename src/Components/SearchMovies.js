b276b76c9b02dfdda0b3d1a7df9f7b9255383428
Michael Robards <mikerobards@icloud.com>
2021-07-29 17:02:51 -0400

    Revert "add api key"
    This reverts commit 295ef48a3a6cf14668248c6c8d35414b66788622.

--- src/Components/SearchMovies.js
+++ src/Components/SearchMovies.js
@@ -1,18 +1,8 @@
 import React from 'react'
-import apiKey from '../key'
-
 
 export default function SearchMovies() {
-
-    const searchMovie = async (e) => {
-        e.preventDefault()
-        console.log("submitting")
-
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`;
-    }
-
     return (
-        <form className="form" onSubmit={searchMovie}>
+        <form className="form">
             <label htmlFor="query" className="label">Movie Name</label>
             <input className="input" type="text" name="query" placeholder="Such as 'Jurassic Park'" />
             <button className="button" type="submit">Search</button>
--- src/key.js
+++
@@ -1,3 +0,0 @@
-const apiKey = "5651886f01ede1990eebd0e1825cdc1c"
-
-export default apiKey
