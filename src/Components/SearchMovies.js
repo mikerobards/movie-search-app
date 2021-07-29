d2df608704893d3cabb16ca45f7087662a403adc
Michael Robards <mikerobards@icloud.com>
2021-07-29 17:24:29 -0400

    fetch api

--- src/Components/SearchMovies.js
+++ src/Components/SearchMovies.js
@@ -7,9 +7,19 @@ export default function SearchMovies() {
     const searchMovies = async (e) => {
         e.preventDefault();
         console.log("submitting");
+        const query = "Jurassic Park"
 
-        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=&page=1&include_adult=false`;
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`;
         console.log(url)
+
+        try {
+            const res = await fetch(url)
+            const data = await res.json()
+            console.log(data)
+        } catch (err) {
+            console.error(err)
+        }
+
     }
 
     return (
