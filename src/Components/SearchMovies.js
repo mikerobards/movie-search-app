b08426462acda0751f22b78c3cafeeb3e2272eaa
Michael Robards <mikerobards@icloud.com>
2021-07-29 16:43:34 -0400

    style form

--- src/Components/SearchMovies.js
+++ src/Components/SearchMovies.js
@@ -3,8 +3,8 @@ import React from 'react'
 export default function SearchMovies() {
     return (
         <form className="form">
-            <label htmlFor="query" className="label">Moive Name</label>
-            <input type="text" name="query" placeholder="Such as 'Jurassic Park'" />
+            <label htmlFor="query" className="label">Movie Name</label>
+            <input className="input" type="text" name="query" placeholder="Such as 'Jurassic Park'" />
             <button className="button" type="submit">Search</button>
         </form>
     )
