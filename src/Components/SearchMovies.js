1ebaf6063f8d971780208ee643ddc2fb86af9539
Michael Robards <mikerobards@icloud.com>
2021-07-30 13:12:41 -0400

    my solution

--- src/Components/SearchMovies.js
+++ src/Components/SearchMovies.js
@@ -1,13 +1,15 @@
-import React from 'react'
-import apiKey from '../config.js';
+import React, { useState } from 'react'
+import apiKey from '../config.js'
 
 
 export default function SearchMovies() {
 
+    // states - input query, movvies
+    const [query, setQuery] = useState('')
+    const [movies, setMovie] = useState([])
+
     const searchMovies = async (e) => {
         e.preventDefault();
-        console.log("submitting");
-        const query = "Jurassic Park"
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=${apiKey}&language=en-US&query=${query}&page=1&include_adult=false`;
         console.log(url)
@@ -16,17 +18,31 @@ export default function SearchMovies() {
             const res = await fetch(url)
             const data = await res.json()
             console.log(data)
+            setMovie(prevMovies => [...prevMovies, data.results[0].title])
         } catch (err) {
             console.error(err)
         }
 
+        console.log(movies)
+
     }
+
+
 
     return (
         <form className="form" onSubmit={searchMovies}>
             <label htmlFor="query" className="label">Movie Name</label>
-            <input className="input" type="text" name="query" placeholder="Such as 'Jurassic Park'" />
+            <input
+                className="input"
+                type="text"
+                name="query"
+                placeholder="Such as 'Jurassic Park'"
+                value={query}
+                onChange={(e) => setQuery(e.target.value)}
+            />
             <button className="button" type="submit">Search</button>
+
+            <h1>{movies} </h1>
         </form>
     )
 }
