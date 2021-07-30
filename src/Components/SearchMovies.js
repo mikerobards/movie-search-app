eac98054adc5514819595821bc9ce77db800aef5
Michael Robards <mikerobards@icloud.com>
2021-07-30 13:44:14 -0400

    display movies after search

--- src/Components/SearchMovies.js
+++ src/Components/SearchMovies.js
@@ -8,6 +8,8 @@ export default function SearchMovies() {
     const [query, setQuery] = useState('')
     const [movies, setMovie] = useState([])
 
+
+
     const searchMovies = async (e) => {
         e.preventDefault();
 
@@ -18,7 +20,8 @@ export default function SearchMovies() {
             const res = await fetch(url)
             const data = await res.json()
             console.log(data)
-            setMovie(prevMovies => [...prevMovies, data.results[0].title])
+            // setMovie(prevMovies => [...prevMovies, data.results[0].title])
+            setMovie(data.results)
         } catch (err) {
             console.error(err)
         }
@@ -30,19 +33,37 @@ export default function SearchMovies() {
 
 
     return (
-        <form className="form" onSubmit={searchMovies}>
-            <label htmlFor="query" className="label">Movie Name</label>
-            <input
-                className="input"
-                type="text"
-                name="query"
-                placeholder="Such as 'Jurassic Park'"
-                value={query}
-                onChange={(e) => setQuery(e.target.value)}
-            />
-            <button className="button" type="submit">Search</button>
+        <>
+            <form className="form" onSubmit={searchMovies}>
+                <label htmlFor="query" className="label">Movie Name</label>
+                <input
+                    className="input"
+                    type="text"
+                    name="query"
+                    placeholder="Such as 'Jurassic Park'"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
+                />
+                <button className="button" type="submit">Search</button>
+            </form>
+            <div className="card-list">
+                {movies.filter(movie => movie.poster_path).map(movie => (
 
-            <h1>{movies} </h1>
-        </form>
+                    <div className="card" key={movie.id}>
+                        <img
+                            className="card--image"
+                            src={`https://image.tmdb.org/t/p/w185_and_h278_bestv2/${movie.poster_path}`}
+                            alt={movie.title + ' poster'}
+                        />
+                        <div className="card--content">
+                            <h3 className="card--title">{movie.title}</h3>
+                            <p><small>RELEASE DATE: {movie.release_date}</small></p>
+                            <p><small>RATING: {movie.vote_average}</small></p>
+                            <p className="card--desc">{movie.overview}</p>
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </>
     )
 }
